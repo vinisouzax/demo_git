4e9f645a4ef5939205f59346a1097ea2d3938c03
vinicius <vinisouzax@gmail.com>
2021-08-18 13:54:51 -0300

    Troca de texto

--- app/src/main/java/com/movep/demogit/MainActivity.java
+++ app/src/main/java/com/movep/demogit/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Trocar com sua própria ação", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
